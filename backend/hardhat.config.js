2a5e8439d8ebb1df62d7706e4a9b13c97651659d
Nicolas Ferreira <nico.ferreira.web@gmail.com>
2024-06-28 14:38:00 +0200

    Config et deploiement sur la blockchain Base

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,7 +1,45 @@
 require("@nomicfoundation/hardhat-toolbox");
 require('@openzeppelin/hardhat-upgrades');
+const { vars } = require("hardhat/config");
+
+const PRIVATE_KEY = vars.get("PRIVATE_KEY");
+const BASESCAN_API_KEY = vars.get("BASESCAN_API_KEY");
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    // for testnet
+    'base-sepolia': {
+      url: 'https://sepolia.base.org',
+      accounts: [PRIVATE_KEY],
+      gasPrice: 1000000000,
+    },
+    // for local dev environment
+    'base-local': {
+      url: 'http://localhost:8545',
+      accounts: [PRIVATE_KEY],
+      gasPrice: 1000000000,
+    },
+  },
+  defaultNetwork: 'hardhat',
+  etherscan: {
+    apiKey: {
+     "base-sepolia": BASESCAN_API_KEY
+    },
+    customChains: [
+      {
+        network: "base-sepolia",
+        chainId: 84532,
+        urls: {
+         apiURL: "https://api-sepolia.basescan.org/api",
+         browserURL: "https://sepolia.basescan.org"
+        }
+      }
+    ]
+  },
+  sourcify: {
+    enabled: true
+  }
 };
--- backend/scripts/deploy.js
+++ backend/scripts/deploy.js
@@ -1,49 +1,51 @@
 const hre = require("hardhat");
+const networkName = hre.network.name;
 
 // Function to convert a string to bytes
 function stringToBytes(inputString) {
-    return hre.ethers.hexlify(hre.ethers.toUtf8Bytes(inputString));
-  }
+  return hre.ethers.hexlify(hre.ethers.toUtf8Bytes(inputString));
+}
 
 async function main() {
   // Get the contract to deploy
   const Ordo = await hre.ethers.deployContract("Ordo");
-await Ordo.waitForDeployment();
-
+  await Ordo.waitForDeployment();
   console.log("Ordo deployed to:", Ordo.target);
 
-  // Define test addresses (replace these with actual addresses for real deployment)
-  const [owner, doctor, pharmacist, patient] = await ethers.getSigners();
+  if (networkName === "localhost") {
+    // Define test addresses (replace these with actual addresses for real deployment)
+    const [owner, doctor, pharmacist, patient] = await ethers.getSigners();
 
-  // Register users with encrypted data
-  const doctorString = JSON.stringify({
-    name: "Dr. John Doe",
-    specialty: "Cardiology",
-    hospital: "General Hospital"
-  })
-  const doctorData = stringToBytes(doctorString);
+    // Register users with encrypted data
+    const doctorString = JSON.stringify({
+      name: "Dr. John Doe",
+      specialty: "Cardiology",
+      hospital: "General Hospital"
+    })
+    const doctorData = stringToBytes(doctorString);
 
-  const pharmacistString= JSON.stringify({
-    name: "Pharmacist Jane Smith",
-    pharmacy: "HealthPlus Pharmacy"
-  });
-  const pharmacistData = stringToBytes(pharmacistString);
+    const pharmacistString = JSON.stringify({
+      name: "Pharmacist Jane Smith",
+      pharmacy: "HealthPlus Pharmacy"
+    });
+    const pharmacistData = stringToBytes(pharmacistString);
 
-  const patientString = JSON.stringify({
-    name: "Patient Bob Brown",
-    dob: "1990-01-01",
-    insurance: "123456789"
-  });
-  const patientData = stringToBytes(patientString);
+    const patientString = JSON.stringify({
+      name: "Patient Bob Brown",
+      dob: "1990-01-01",
+      insurance: "123456789"
+    });
+    const patientData = stringToBytes(patientString);
 
-  await Ordo.registerUser(doctor.address, 1, doctorData);
-  console.log("Doctor registered:", doctor.address);
+    await Ordo.registerUser(doctor.address, 1, doctorData);
+    console.log("Doctor registered:", doctor.address);
 
-  await Ordo.registerUser(pharmacist.address, 2, pharmacistData);
-  console.log("Pharmacist registered:", pharmacist.address);
+    await Ordo.registerUser(pharmacist.address, 2, pharmacistData);
+    console.log("Pharmacist registered:", pharmacist.address);
 
-  await Ordo.registerUser(patient.address, 3, patientData);
-  console.log("Patient registered:", patient.address);
+    await Ordo.registerUser(patient.address, 3, patientData);
+    console.log("Patient registered:", patient.address);
+  }
 }
 
 main()
